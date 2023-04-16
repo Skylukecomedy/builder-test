496a785182d4a0fbfdc4bd401c8ea455a817271e
Skylukecomedy <130338499+Skylukecomedy@users.noreply.github.com>
2023-04-16 02:51:04 -0400

    Create buildstest.js

---
+++ buildstest.js
@@ -0,0 +1,8 @@
+export const Builds = [
+  {"Char": "Mario", "Gear": "0000", "Str": "11", "Spd": "12", "Shot": "14", "Pass": "10", "Tech": "16"},
+  {"Char": "Mario", "Gear": "0001", "Str": "13", "Spd": "10", "Shot": "14", "Pass": "10", "Tech": "16"},
+  {"Char": "Mario", "Gear": "0002", "Str": "11", "Spd": "14", "Shot": "12", "Pass": "10", "Tech": "16"},
+  {"Char": "Mario", "Gear": "0003", "Str": "11", "Spd": "12", "Shot": "16", "Pass": "8", "Tech": "16"},
+];
+
+export default Builds;
