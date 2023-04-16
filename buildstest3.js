b58cf8d2929a4de8f02e9b5559b96836e9e325b9
Skylukecomedy <130338499+Skylukecomedy@users.noreply.github.com>
2023-04-16 08:04:53 -0400

    Update buildstest3.js

--- buildstest3.js
+++ buildstest3.js
@@ -3,8 +3,6 @@ const Builds = [
   {"Char": "Mario", "Gear": "0001", "Str": "13", "Spd": "10", "Shot": "14", "Pass": "10", "Tech": "16"},
   {"Char": "Mario", "Gear": "0002", "Str": "11", "Spd": "14", "Shot": "12", "Pass": "10", "Tech": "16"},
   {"Char": "Mario", "Gear": "0003", "Str": "11", "Spd": "12", "Shot": "16", "Pass": "8", "Tech": "16"},
-
-"11", "Spd": "14", "Shot": "12", "Pass": "10", "Tech": "16"},
 {"Char": "Mario", "Gear": "0003", "Str": "11", "Spd": "12", "Shot": "16", "Pass": "8", "Tech": "16"},
 {"Char": "Mario", "Gear": "0004", "Str": "9", "Spd": "12", "Shot": "14", "Pass": "12", "Tech": "16"},
 {"Char": "Mario", "Gear": "0005", "Str": "11", "Spd": "12", "Shot": "12", "Pass": "10", "Tech": "18"},
