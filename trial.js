caf1ccb390aba9bfe724055b64aa653dc092e46b
Skylukecomedy <130338499+Skylukecomedy@users.noreply.github.com>
2023-04-18 02:06:48 -0400

    Create trial.js

---
+++ trial.js
@@ -0,0 +1,15 @@
+const Builds = [
+{"Char": "Mario", "Gear": "0000", "Str": "11", "Spd": "12", "Shot": "14", "Pass": "10", "Tech": "16"},
+{"Char": "Mario", "Gear": "0001", "Str": "13", "Spd": "10", "Shot": "14", "Pass": "10", "Tech": "16"},
+{"Char": "Mario", "Gear": "0002", "Str": "11", "Spd": "14", "Shot": "12", "Pass": "10", "Tech": "16"},
+{"Char": "Mario", "Gear": "0003", "Str": "11", "Spd": "12", "Shot": "16", "Pass": "8", "Tech": "16"},
+{"Char": "Mario", "Gear": "0004", "Str": "9", "Spd": "12", "Shot": "14", "Pass": "12", "Tech": "16"},
+{"Char": "Mario", "Gear": "0005", "Str": "11", "Spd": "12", "Shot": "12", "Pass": "10", "Tech": "18"},
+{"Char": "Mario", "Gear": "0006", "Str": "10", "Spd": "16", "Shot": "13", "Pass": "9", "Tech": "15"},
+{"Char": "Mario", "Gear": "0007", "Str": "13", "Spd": "10", "Shot": "16", "Pass": "9", "Tech": "15"},
+{"Char": "Mario", "Gear": "0008", "Str": "12", "Spd": "10", "Shot": "16", "Pass": "11", "Tech": "14"},
+{"Char": "Mario", "Gear": "0009", "Str": "9", "Spd": "14", "Shot": "12", "Pass": "11", "Tech": "17"},
+{"Char": "Mario", "Gear": "0010", "Str": "13", "Spd": "12", "Shot": "12", "Pass": "10", "Tech": "16"},
+{"Char": "Mario", "Gear": "0011", "Str": "15", "Spd": "10", "Shot": "12", "Pass": "10", "Tech": "16"},
+{"Char": "Mario", "Gear": "0012", "Str": "13", "Spd": "14", "Shot": "10", "Pass": "10", "Tech": "16"},
+];
