f01c3d87bb248f6d5fcfeed5961006a2004d8f69
Skylukecomedy <130338499+Skylukecomedy@users.noreply.github.com>
2023-09-11 03:03:00 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,33 +3,34 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
-
-
-
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
 // close menu when you click on a menu item 
 menu_items.forEach(item => {
-    item.addEventListener('click',function(event){
-       event.preventDefault();
+    item.addEventListener('click', function (event) {
+        event.preventDefault();
         close();
-        scrollToTop();
     })
 })
 
 function show() {
-    const scrollY = window.scrollY;
-    mainMenu.style.top = `${scrollY}px`;
     mainMenu.style.display = 'flex';
+    mainMenu.style.position = 'fixed';
+    mainMenu.style.top = '0';
+    mainMenu.style.left = '0';
+    mainMenu.style.right = '0';
+    mainMenu.style.bottom = '0';
 }
-function close(){
-    mainMenu.style.top = '-100%';
+
+function close() {
+    mainMenu.style.position = 'initial'; // Reset the position
+    mainMenu.style.display = 'none';
     openMenu.style.display = 'block'; // Show the hamburger button
     closeMenu.style.display = 'none'; // Hide the close button
     location.reload();
 }
-    
+
 function scrollToTop() {
     window.scrollTo({
         top: 0,
